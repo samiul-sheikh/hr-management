0c9fd3cfac02f1a8cb8ea2d006020ea7e0ce0adb
samiul-sheikh <sheikh.samiul1971@gmail.com>
2022-03-31 23:57:52 +0600

    add new employee

---
+++ src/pages/Homepage/AddEmployee/AddEmployee.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const AddEmployee = () => {
+  return (
+    <div>
+      <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              First Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+            {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+              Last Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+              Email Address
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="text" placeholder="something@gmail.com" />
+            <p class="text-gray-600 text-xs italic">Enter a valid email address</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-phone">
+              Phone Number
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-phone" type="number" placeholder="0178409...." />
+            <p class="text-gray-600 text-xs italic">Enter a valid phone number</p>
+          </div>
+        </div>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddEmployee;
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddEmployee from './AddEmployee/AddEmployee';
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <AddEmployee />
+    </div>
+  );
+};
+
+export default Homepage;
